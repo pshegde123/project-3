58aae4316a0243cf4a6e8896615e1972053b0e3d
christian colon <christiancolon152@gmail.com>
2020-01-25 13:41:38 -0500

    added css changed styling and font

--- client/src/components/GenreCard/index.js
+++ client/src/components/GenreCard/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style.css'
 import {
   Card, Button, CardImg, CardTitle, CardText, CardColumns,
   CardSubtitle, CardBody, CardImgOverlay
@@ -8,7 +9,7 @@ import {
 function GenreCard(props){
     return(
         <Card> 
-        <CardImg  src={props.image} alt="Card image cap"/>
+        <CardImg style={{size:'50px'}} src={props.image} alt="Card image cap"/>
         <CardBody>
           <CardTitle style={{fontSize:'25px'}}>{props.name}</CardTitle>
           <CardSubtitle>Games count:{props.games_count}</CardSubtitle>
