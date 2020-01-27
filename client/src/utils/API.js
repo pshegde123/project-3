bec2691811988543c241e411b9040119777a6d1f
pradnya <pradnyahegde@gmail.com>
2020-01-27 11:53:24 -0500

    added method getAllGames

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -22,5 +22,16 @@ export default {
                 "x-rapidapi-key": "91048f5edcmshbb3ad5482ca6475p1f5763jsn3f223f4eabf9"
             }
         })
+    },
+    getAllGames:function(){
+        return axios({
+            "method":"GET",
+            "url":"https://rawg-video-games-database.p.rapidapi.com/games",
+            "headers":{
+            "content-type":"application/octet-stream",
+            "x-rapidapi-host":"rawg-video-games-database.p.rapidapi.com",
+            "x-rapidapi-key":"91048f5edcmshbb3ad5482ca6475p1f5763jsn3f223f4eabf9"
+            }
+            })
     }
 };
