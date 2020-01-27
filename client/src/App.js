08b48e45a8c1ab0c11a88e9104db12eaf80ba3e5
pradnya <pradnyahegde@gmail.com>
2020-01-27 11:54:43 -0500

    added router link for Games component

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,17 @@ import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import Genre from './components/pages/Genre';
 import Platforms from './components/pages/Platforms';
+import Games from './components/pages/Games';
 
 function App() {
   return (
       <Router>
         <div>
           <Navbar/>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={Home}/>
           <Route exact path="/genre" component={Genre}/>
           <Route exact path="/platforms" component={Platforms}/>
+          <Route exact path="/games" component={Games}/>
         </div>
       </Router>
   );
