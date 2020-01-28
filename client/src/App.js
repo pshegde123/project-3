0fdb414bf1ea270304f0315c75a4342b943a9d42
christian colon <christiancolon152@gmail.com>
2020-01-28 17:47:59 -0500

    added link to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import {BrowserRouter as Router,Route} from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import Genre from './components/pages/Genre';
-
+import Games from './components/Games'
 function App() {
   return (
       <Router>
         <div>
           <Navbar/>
+          <Route exact path="/games" component={Games}/>
           <Route exact path="/" component={Home} />
           <Route exact path="/genre" component={Genre}/>
         </div>
