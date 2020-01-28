7fe698b8428ac89978d0dfbeb98320125f5067e4
pradnya <pradnyahegde@gmail.com>
2020-01-27 22:52:22 -0500

    added reactplayer component

---
+++ client/src/components/GameCard/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+    Card, Button, CardImg, CardTitle, CardText, CardSubtitle, CardBody
+} from 'reactstrap';
+import ReactPlayer from 'react-player';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function GameCard(props) {
+    const platforms = props.platforms;
+    const names = [];
+    for (let iter = 0; iter < platforms.length; iter++) {
+        names[iter] = platforms[iter].platform.name;
+    }
+    return (
+        <Card>
+            <ReactPlayer
+                url={props.url}
+                className='react-player'
+                playing={false}
+                muted={true}
+                controls={true}
+                height='250px'
+                width='400px' />
+            <CardBody>
+                <CardTitle style={{ fontSize: '25px' }}>{props.name}</CardTitle>
+                <CardSubtitle><li>Ratings: {props.rating}</li><li>MetaScrore:{props.metascore}</li></CardSubtitle>
+                <CardText>
+                    {names.map(name =>
+                        <FontAwesomeIcon icon={['fab', 'windows']}/>
+                    )}
+                </CardText>
+                <Button>+{props.users}</Button>
+            </CardBody>
+        </Card>
+    );
+}
+
+export default GameCard;
