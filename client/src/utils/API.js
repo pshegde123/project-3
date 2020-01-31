14d08025fa3173b8af59d1a516df7b13b7e20abc
pradnya <pradnyahegde@gmail.com>
2020-01-30 21:06:51 -0500

    added method getAutocompleteResult

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,37 +1,37 @@
 import axios from "axios";
+const userHeader = {
+    "content-type": "application/octet-stream",
+    "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+    "x-rapidapi-key": "91048f5edcmshbb3ad5482ca6475p1f5763jsn3f223f4eabf9"
+};
 
 export default {
     getAllGenres: function () {
         return axios({
             "method": "GET",
             "url": "https://rawg-video-games-database.p.rapidapi.com/genres",
-            "headers": {
-                "content-type": "application/octet-stream",
-                "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
-                "x-rapidapi-key": "91048f5edcmshbb3ad5482ca6475p1f5763jsn3f223f4eabf9"
-            }
+            "headers": userHeader
         })
     },
     getAllPlatforms: function () {
         return axios({
             "method": "GET",
             "url": "https://rawg-video-games-database.p.rapidapi.com/platforms",
-            "headers": {
-                "content-type": "application/octet-stream",
-                "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
-                "x-rapidapi-key": "91048f5edcmshbb3ad5482ca6475p1f5763jsn3f223f4eabf9"
-            }
+            "headers": userHeader
         })
     },
     getAllGames:function(){
         return axios({
             "method":"GET",
             "url":"https://rawg-video-games-database.p.rapidapi.com/games",
-            "headers":{
-            "content-type":"application/octet-stream",
-            "x-rapidapi-host":"rawg-video-games-database.p.rapidapi.com",
-            "x-rapidapi-key":"91048f5edcmshbb3ad5482ca6475p1f5763jsn3f223f4eabf9"
-            }
-            })
+            "headers": userHeader
+        })
+    },
+    getAutocompleteResult:function(query){
+        return axios({
+            "method":"GET",
+            "url":"https://rawg-video-games-database.p.rapidapi.com/games?search="+query,
+            "headers": userHeader
+        })
     }
 };
